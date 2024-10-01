b0447d84b5d56bea851af7e802b3a5fec147c689
EduFukui <fukuigreninja0001@gmail.com>
2024-09-30 21:53:51 -0300

    Add files via upload

---
+++ Eduardo/UC2 - Algoritmos/30-09/Exercicios1.js
@@ -0,0 +1,10 @@
+// Exercicios
+
+let primeiroNome = "Eduardo"
+let sobrenome = "Fukui"
+let idade = "16"
+let souEstudante = true
+
+console.log("")
+console.log("Meu nome é", primeiroNome, sobrenome, "e tenho",idade,"anos. Eu sou estudante?", souEstudante)
+console.log("")
---
+++ Eduardo/UC2 - Algoritmos/30-09/exercicio2.js
@@ -0,0 +1,6 @@
+
+let primeiroNome = 'Eduardo'
+let filmeFavorito = 'Seishun Buta Yarou Wa Yumemiru Shoujo'
+let musicaFavorita = 'Fukashigi no Karte'
+
+console.log(`Meu nome é ${primeiroNome}, meu filme favorito é o ${filmeFavorito} e minha musica favorita é ${musicaFavorita}.`)
